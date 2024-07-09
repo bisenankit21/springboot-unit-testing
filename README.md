28be2e2037ec98d72734358edd99500dc14ad813
KarunaBisen <karunabisen19@gmail.com>
2024-07-09 11:22:47 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -2,4 +2,31 @@
 Unit test have following structures
 1. Set Up  --create  instance of the class
 2. Execute  --call the method you want to test
-3. Assert   --check the result and verify that it is the expected result
+3. Assert   --check the result and verify that it is the expected result 
+
+assertEquals = assert that the values are equals
+assertNotEquals = assert that the values are not euqal
+assertNull= Assert that the values are null
+assertNotNull = Assert that the value is not null
+ format : assertEquals(expected, actual, optional message)
+
+dependency we need to add :-
+<dependency>
+<groupId>org.junit.jupiter</groupId>
+<artifactId>junit-jupiter</artifactId>
+<version>5.10.3</version>
+<scope>test</scope>
+</dependency>
+
+
+
+we need to do some common operation like
+Lifecycle method annotation :
+1. @BeforeEach  - Method is executed before each test method(useful for common set up code : creating object, setting up test data)
+2. @AfterEach - Method is executed after each test method ( Useful for common clean up code: releasing resources, cleaning up test data)
+3. @BeforeAll- Method is executed only once, before all test methods.( useful for getting database connection, connecting to servers...)
+4. @AfterAll - Method is executed only once, after all test methods(useful for releasing database connection, disconnecibg from servers)
+5. NOte- @BeforeAll and @AfterALl method are by defoult static 
+
+
+
--- junitDemo/src/test/java/com/luc2code/junitdemo/DemoUtilsTest.java
+++ junitDemo/src/test/java/com/luc2code/junitdemo/DemoUtilsTest.java
@@ -1,21 +1,44 @@
 package com.luc2code.junitdemo;
 
 import com.luv2code.junitdemo.DemoUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
+    DemoUtils demoUtils;
+    @BeforeEach
+    void setBeforeEach(){
+        demoUtils=new DemoUtils();
+        System.out.println("@BeforeEac executes before the execution of of each method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("AFter each execute after each emthod");
+        System.out.println();
+    }
+    @BeforeAll
+    static void setUpBeforeachClass(){
+        System.out.println("@BeforeAll executes only once before all test emthod in the class");
+    }
+    @AfterAll
+    static void setUpAftereachClass(){
+        System.out.println("@AfterAll executes only once after  all test emthod in the class");
+    }
+
     @Test
     void testEqualsAndNotEquals(){
-        DemoUtils demoUtils=new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
+
         assertEquals(6,demoUtils.add(2,4),"2+4 must be equals to ");
         assertNotEquals(6,demoUtils.add(1,9),"1+9 must noy be 6");
 
     }
     @Test
     void testNullAndNotNull(){
-        DemoUtils demoUtils=new DemoUtils();
+        System.out.println("Running test: testNullAndNotNull");
+
         String str1=null;
         String str2="Ankit";
         assertNull(demoUtils.checkNull(str1),"Object should be null");
